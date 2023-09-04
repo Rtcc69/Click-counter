b918f74f4d340132bfa2bd7e0829f6f318d40fca
Rtcc69 <ramonteixeira2005@gmail.com>
2023-09-04 12:06:51 -0300

    Fist commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let res = document.getElementById('res')
+
+let atualValue = Number(res.value)
+
+function addOne(){
+   let addOne = atualValue += 1 
+   res.value = addOne
+    
+     
+}
+
+function rmOne(){
+   let rmOne = atualValue -= 1
+   res.value =
+}
