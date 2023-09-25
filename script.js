b91ccc58062c961047124807873c45f6d34eb622
Rtcc69 <ramonteixeira2005@gmail.com>
2023-09-25 09:44:14 -0300

    Keybord features

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-let res = document.getElementById('res')
+let res = document.getElementById("res");
 
-let atualValue = Number(res.value)
+let atualValue = Number(res.value);
 
-function addOne(){
-   let addOne = atualValue += 1 
-   res.value = addOne
-    
-     
+function addOne() {
+  // mais um
+  let addOne = (atualValue += 1);
+  res.value = addOne;
 }
 
-function rmOne(){
-   let removeOne = atualValue -=1
-   res.value= removeOne
+document.addEventListener("keydown", function (event) {
+  // ouvinte do evento ao apertar uma tecla
+  if (event.keyCode === 39) {
+    //  verifica a tecla apertada (39 é o codigo da tecla direita)
+    addOne();
+  }
+});
+
+function rmOne() {
+  // menos um
+  let removeOne = (atualValue -= 1);
+  res.value = removeOne;
 }
 
-function clearCounter(){
-   res.value = 0
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode === 37) {
+    rmOne();
+  }
+});
+
+function clearCounter() {
+  res.value = 0;
 }
-   
